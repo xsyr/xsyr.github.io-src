37ccd13d2c99e612c073ee922c07ab0c99552737
xsyr <xinsuiyuer@gmail.com>
2017-09-07 23:04:48 +0800

    update

--- deploy.sh
+++ deploy.sh
@@ -1,7 +1,33 @@
 #!/bin/bash
 
+CUR_DIR=$(pwd)
+
+SRC="${BASH_SOURCE[0]}"
+SRC_DIR=$(dirname $SRC)
+
+GIT_DIR=$SRC_DIR/xsyr.github.io
+SITE_DIR=$SRC_DIR/_site
+
+
+if [ ! -d $SITE_DIR ]; then
+    git clone git@github.com:xsyr/xsyr.github.io.git
+    if [ $? -ne 0 ]; then
+        echo "clone from git failed."
+        exit 1
+    fi
+
+    mv $GIT_DIR $SITE_DIR
+fi
+
 jekyll clean
 jekyll b
+
+cd $SITE_DIR
+
 git add .
 git commit -am 'update'
 git push
+
+cd $CUR_DIR
+
+
